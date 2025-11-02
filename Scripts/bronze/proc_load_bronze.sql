372dbcbeb3da560490d773a5322626f20d418456
Zuunhii <121943337+Zuunhii@users.noreply.github.com>
2025-11-02 21:36:39 +0700

    Clean up comments in proc_load_bronze.sql
    Removed comments and documentation from the stored procedure.

--- Scripts/bronze/proc_load_bronze.sql
+++ Scripts/bronze/proc_load_bronze.sql
@@ -1,21 +1,4 @@
-/*
-===============================================================================
-Stored Procedure: Load Bronze Layer (Source -> Bronze)
-===============================================================================
-Script Purpose:
-    This stored procedure loads data into the 'bronze' schema from external CSV files. 
-    It performs the following actions:
-    - Truncates the bronze tables before loading data.
-    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
 
-Parameters:
-    None. 
-	  This stored procedure does not accept any parameters or return any values.
-
-Usage Example:
-    EXEC bronze.load_bronze;
-===============================================================================
-*/
 Create or alter procedure bronze.proc_load_bronze AS
 begin 
 	declare @start_time DATETIME, @end_time DATETIME;
